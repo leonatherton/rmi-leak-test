7ca8063292743dcd5d1c619788071f097cff5540
Leon Atherton <leon@idrsolutions.com>
2023-10-18 12:53:57 +0100

    Fix intermittent failures to reproduce

--- src/main/java/rmi/RmiLeakTestServlet.java
+++ src/main/java/rmi/RmiLeakTestServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import java.rmi.AlreadyBoundException;
+import java.rmi.NoSuchObjectException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -14,24 +15,40 @@ import java.util.logging.Logger;
 @WebServlet(name = "RMI Leak Test Servlet", urlPatterns = "/*", loadOnStartup = 1)
 public class RmiLeakTestServlet extends HttpServlet {
 
+    private static Registry registry;
+    private static MyRemoteClass myRemoteClass;
+
     @Override
     public void init() throws ServletException {
         super.init();
 
         try {
-            final Registry registry = LocateRegistry.createRegistry(0);
+            Logger.getLogger(RmiLeakTestServlet.class.getName()).log(Level.INFO, "Init leak test");
 
-            final MyRemoteClass myRemoteClass = new MyRemoteClass();
+            registry = LocateRegistry.createRegistry(0);
+
+            myRemoteClass = new MyRemoteClass();
 
             final MyRemote stub = (MyRemote) UnicastRemoteObject.exportObject(myRemoteClass, 0);
 
             registry.bind("MyStub", stub);
 
+        } catch (final RemoteException | AlreadyBoundException e) {
+            Logger.getLogger(RmiLeakTestServlet.class.getName()).log(Level.SEVERE, "Init Exception", e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+
+        try {
+            Logger.getLogger(RmiLeakTestServlet.class.getName()).log(Level.INFO, "Destroy leak test");
+
 //            UnicastRemoteObject.unexportObject(myRemoteClass, true); // Shutdown succeeds with this
             UnicastRemoteObject.unexportObject(registry, true);
-
-        } catch (final RemoteException | AlreadyBoundException e) {
-            Logger.getLogger(RmiLeakTestServlet.class.getName()).log(Level.SEVERE, "Exception", e);
+        } catch (final NoSuchObjectException e) {
+            Logger.getLogger(RmiLeakTestServlet.class.getName()).log(Level.SEVERE, "Destroy Exception", e);
         }
     }
 }
